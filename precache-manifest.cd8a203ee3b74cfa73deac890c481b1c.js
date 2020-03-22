02261eed21e1ef97779488b81ea911a39faac103
Joy Song <joy@Joyui-MacBookPro.local>
2020-03-22 23:38:04 +0900

    Updates

---
+++ precache-manifest.cd8a203ee3b74cfa73deac890c481b1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd702384539379405e0722d38c457af",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "fe3afba4e01900148428",
+    "url": "/movie_app_2020/static/css/main.82ac6c56.chunk.css"
+  },
+  {
+    "revision": "0e16d3f86d786e10ecef",
+    "url": "/movie_app_2020/static/js/2.844389f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.844389f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe3afba4e01900148428",
+    "url": "/movie_app_2020/static/js/main.8033cb22.chunk.js"
+  },
+  {
+    "revision": "d5ba99470fbd5d5276a4",
+    "url": "/movie_app_2020/static/js/runtime-main.6a93e83b.js"
+  }
+]);
